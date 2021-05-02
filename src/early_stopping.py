8f2fb13ae4ce12a3796b4cc14066fee748525073
ananyahjha93 <ananya@pytorchlightning.ai>
2021-05-02 18:41:30 -0400

    early stopping bug

--- src/early_stopping.py
+++ src/early_stopping.py
@@ -9,51 +9,11 @@ from pytorch_lightning.utilities.exceptions import MisconfigurationException
 
 
 class EarlyStopping(Callback):
-    r"""
-    Monitor a metric and stop training when it stops improving.
-
-    Args:
-        monitor: quantity to be monitored.
-        min_delta: minimum change in the monitored quantity to qualify as an improvement, i.e. an absolute
-            change of less than `min_delta`, will count as no improvement.
-        patience: number of validation checks with no improvement
-            after which training will be stopped. Under the default configuration, one validation check happens after
-            every training epoch. However, the frequency of validation can be modified by setting various parameters on
-            the ``Trainer``, for example ``check_val_every_n_epoch`` and ``val_check_interval``.
-
-            .. note::
-
-                It must be noted that the patience parameter counts the number of validation checks with
-                no improvement, and not the number of training epochs. Therefore, with parameters
-                ``check_val_every_n_epoch=10`` and ``patience=3``, the trainer will perform at least 40 training
-                epochs before being stopped.
-
-        verbose: verbosity mode.
-        mode: one of ``'min'``, ``'max'``. In ``'min'`` mode, training will stop when the quantity
-            monitored has stopped decreasing and in ``'max'`` mode it will stop when the quantity
-            monitored has stopped increasing.
-        strict: whether to crash the training if `monitor` is not found in the validation metrics.
-
-    Raises:
-        MisconfigurationException:
-            If ``mode`` is none of ``"min"`` or ``"max"``.
-        RuntimeError:
-            If the metric ``monitor`` is not available.
-
-    Example::
-
-        >>> from pytorch_lightning import Trainer
-        >>> from pytorch_lightning.callbacks import EarlyStopping
-        >>> early_stopping = EarlyStopping('val_loss')
-        >>> trainer = Trainer(callbacks=[early_stopping])
-    """
     mode_dict = {
         'min': torch.lt,
         'max': torch.gt,
     }
 
-    # TODO: in the general case, we take an equation like einops
-    # TODO: and a list of monitor, min_delta and mode
     def __init__(
         self,
         monitor1: str = 'early_stop_on',
@@ -67,32 +27,41 @@ class EarlyStopping(Callback):
         strict: bool = True,
     ):
         super().__init__()
-        self.monitor = monitor
+        self.monitor1 = monitor1
+        self.monitor2 = monitor2
         self.patience = patience
         self.verbose = verbose
         self.strict = strict
-        self.min_delta = min_delta
-        self.wait_count = 0
+        self.min_delta1 = min_delta1
+        self.min_delta2 = min_delta2
+        self.wait_count1 = 0
+        self.wait_count2 = 0
         self.stopped_epoch = 0
-        self.mode = mode
+        self.mode1 = mode1
+        self.mode2 = mode2
 
-        if self.mode not in self.mode_dict:
-            raise MisconfigurationException(f"`mode` can be {', '.join(self.mode_dict.keys())}, got {self.mode}")
+        if (self.mode1 not in self.mode_dict) or (self.mode2 not in self.mode_dict):
+            raise MisconfigurationException(f"`mode` can be {', '.join(self.mode_dict.keys())}")
 
-        self.min_delta *= 1 if self.monitor_op == torch.gt else -1
         torch_inf = torch.tensor(np.Inf)
-        self.best_score = torch_inf if self.monitor_op == torch.lt else -torch_inf
+
+        self.min_delta1 *= 1 if self.monitor_op1 == torch.gt else -1
+        self.min_delta2 *= 1 if self.monitor_op2 == torch.gt else -1
+
+        self.best_score1 = torch_inf if self.monitor_op1 == torch.lt else -torch_inf
+        self.best_score2 = torch_inf if self.monitor_op2 == torch.lt else -torch_inf
 
     def _validate_condition_metric(self, logs):
-        monitor_val = logs.get(self.monitor)
+        monitor_val1 = logs.get(self.monitor1)
+        monitor_val2 = logs.get(self.monitor2)
 
         error_msg = (
-            f'Early stopping conditioned on metric `{self.monitor}` which is not available.'
+            f'Early stopping conditioned on metric `{self.monitor1}` and `{self.monitor2}` which is not available.'
             ' Pass in or modify your `EarlyStopping` callback to use any of the following:'
             f' `{"`, `".join(list(logs.keys()))}`'
         )
 
-        if monitor_val is None:
+        if monitor_val1 is None or monitor_val2 is None:
             if self.strict:
                 raise RuntimeError(error_msg)
             if self.verbose > 0:
@@ -103,21 +72,29 @@ class EarlyStopping(Callback):
         return True
 
     @property
-    def monitor_op(self):
-        return self.mode_dict[self.mode]
+    def monitor_op1(self):
+        return self.mode_dict[self.mode1]
+
+    @property
+    def monitor_op2(self):
+        return self.mode_dict[self.mode2]
 
     def on_save_checkpoint(self, trainer, pl_module, checkpoint: Dict[str, Any]) -> Dict[str, Any]:
         return {
-            'wait_count': self.wait_count,
+            'wait_count1': self.wait_count1,
+            'wait_count2': self.wait_count2,
             'stopped_epoch': self.stopped_epoch,
-            'best_score': self.best_score,
+            'best_score1': self.best_score1,
+            'best_score2': self.best_score2,
             'patience': self.patience
         }
 
     def on_load_checkpoint(self, callback_state: Dict[str, Any]):
-        self.wait_count = callback_state['wait_count']
+        self.wait_count1 = callback_state['wait_count1']
+        self.wait_count2 = callback_state['wait_count2']
         self.stopped_epoch = callback_state['stopped_epoch']
-        self.best_score = callback_state['best_score']
+        self.best_score1 = callback_state['best_score1']
+        self.best_score2 = callback_state['best_score2']
         self.patience = callback_state['patience']
 
     def on_validation_end(self, trainer, pl_module):
@@ -140,20 +117,29 @@ class EarlyStopping(Callback):
         ):
             return  # short circuit if metric not present
 
-        current = logs.get(self.monitor)
+        current1 = logs.get(self.monitor1)
+        current2 = logs.get(self.monitor2)
 
         # when in dev debugging
-        trainer.dev_debugger.track_early_stopping_history(self, current)
+        # comment out for multiple metrics for now
+        # trainer.dev_debugger.track_early_stopping_history(self, current)
 
-        if self.monitor_op(current - self.min_delta, self.best_score):
-            self.best_score = current
-            self.wait_count = 0
+        if self.monitor_op1(current1 - self.min_delta1, self.best_score1):
+            self.best_score1 = current1
+            self.wait_count1 = 0
         else:
-            self.wait_count += 1
+            self.wait_count1 += 1
+        
+        if self.monitor_op2(current2 - self.min_delta2, self.best_score2):
+            self.best_score2 = current2
+            self.wait_count2 = 0
+        else:
+            self.wait_count2 += 1
 
-            if self.wait_count >= self.patience:
-                self.stopped_epoch = trainer.current_epoch
-                trainer.should_stop = True
+        # "and" logic for 2 metrics
+        if self.wait_count1 >= self.patience and self.wait_count2 >= self.patience:
+            self.stopped_epoch = trainer.current_epoch
+            trainer.should_stop = True
 
         # stop every ddp process if any world process decides to stop
         trainer.should_stop = trainer.training_type_plugin.reduce_boolean_decision(trainer.should_stop)
