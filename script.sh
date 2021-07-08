f4cb22c05286a94d0bf64d8b80b7d73137b97d34
Ananya Harsh Jha <ananya@pytorchlightning.ai>
2021-07-08 04:05:25 -0400

    imagenet

--- script.sh
+++ script.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-# can set min, max and n
-LOG_SCALE=$(awk -v n=8 -v min=-5 -v max=2 -v seed="$RANDOM" 'BEGIN { srand(seed); for (i=0; i<n; ++i) print rand() * (max - min) + min }')
-LOG_SCALE=($(echo ${LOG_SCALE=[*]}| tr " " "\n" | sort -n))
-LR=1e-4
-KL=1e-3
+CUDA_VISIBLE_DEVICES=0,1,2,3 python src/vae.py --dataset imagenet --data_path /home/ananya/imagenet/data/imagenet_2012 --seed $(date +%s) --online_ft --denoising --gpus 4 --max_epochs 1600 --val_samples 1 --kl_coeff 0 --log_scale 0 --num_workers 16 --batch_size 128 --warmup_epochs 10 --learning_rate 5e-4
 
-for i in {0..7}; do
-    screen -dmS "run$i" bash -c "source /home/ananya/env/bin/activate; python setup.py install; CUDA_VISIBLE_DEVICES=$i python src/vae.py --seed $(date +%s) --online_ft --gpus 1 --max_epochs 4000 --batch_size 256 --warmup_epochs 10 --val_samples 16 --weight_decay 0 --learning_rate $LR --kl_coeff $KL --log_scale ${LOG_SCALE[$i]}; exec sh"
-    sleep 5
-done
+CUDA_VISIBLE_DEVICES=4,5 python src/vae.py --dataset imagenet --data_path /home/ananya/imagenet/data/imagenet_2012 --seed $(date +%s) --online_ft --denoising --gpus 2 --max_epochs 1600 --val_samples 1 --kl_coeff 0 --log_scale 0 --num_workers 16 --batch_size 128 --warmup_epochs 5 --learning_rate 2.5e-4
+
+CUDA_VISIBLE_DEVICES=6 python src/vae.py --dataset imagenet --data_path /home/ananya/imagenet/data/imagenet_2012 --seed $(date +%s) --online_ft --denoising --gpus 1 --max_epochs 800 --val_samples 1 --kl_coeff 0 --log_scale 0 --num_workers 16 --batch_size 128 --warmup_epochs 3 --learning_rate 1e-4
+
+CUDA_VISIBLE_DEVICES=7 python src/vae.py --dataset imagenet --data_path /home/ananya/imagenet/data/imagenet_2012 --seed $(date +%s) --online_ft --denoising --gpus 1 --max_epochs 800 --val_samples 1 --kl_coeff 0 --log_scale 0 --num_workers 16 --batch_size 128 --warmup_epochs 3 --learning_rate 5e-4
